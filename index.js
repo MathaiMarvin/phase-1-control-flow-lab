ade510713b38be1a0ad4e520785124f54fc56f72
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2022-11-23 14:55:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  
+  let result
+  if(someValue<=400){
+    result = "This one is on me!";
+  }else if(someValue>2000 && someValue<2500){
+    result = "I will gladly take your thirty bucks.";
+  }else if (someValue>2500){
+    result = "No can do.";
+  }
+  return result
 }
+scuberGreetingForFeet(2501);
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result
+  if(city==="NYC"){
+    result = "Ok, sounds good."
+  }else if(city!=="NYC"){
+    result ="No go.";
+  }
+  return result
 }
+ternaryCheckCity('NYC');
+
+function switchOnCharmFromTip(response){
+  // Write your code here!
+  let result
+  if(response=='generous'){
+    result = "Thank you so much."
+
+  }else if(response=='not as generous'){
+    result = "Thank you.";
+  }else{
+    result="Bye."
+  }
+  return result
+}
+switchOnCharmFromTip();
